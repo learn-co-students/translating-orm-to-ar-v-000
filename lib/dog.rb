261faa6361112a31a0b2f98b63956727170de0ae
Leonardo Guzman <lguzman@me.com>
2017-07-18 06:01:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,23 @@
-class Dog
+#Dog class inherits from ActiveRecord::Base
+# By using active record gem, we inturn inherit all the methods with it.
+=begin
+inheritence
+    inherits from ActiveRecord::Base
+  attributes
+    has a name and a breed
+  .create
+    takes in a hash of attributes and uses metaprogramming to create a new dog object. Then it uses the #save method to save that dog to the data
+  .save
+    saves an instance of the dog class to the database and then sets the given dogs `id` attribute
+  .update
+    updates the record associated with a given instance
+  .find_or_create_by
+    creates a dog if it does not already exist
+  .find_by_name
+    returns a dog that matches the name from the DB
+  .find_by_id
+    returns a dog that matches the name from the DB
+=end
+class Dog < ActiveRecord::Base
+
 end
