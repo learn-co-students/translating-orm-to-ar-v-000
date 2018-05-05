325d17ffdbe5339efe683f320b4fae6932831769
osxgirl <osxgirl@mac.com>
2018-05-05 12:56:46 -0400

    //❤️

--- bin/environment.rb
+++ bin/environment.rb
@@ -10,8 +10,7 @@ DB = ActiveRecord::Base.establish_connection(
       :adapter => "sqlite3",
       :database => "../db/dogs"
     )
-
-  DB = ActiveRecord::Base.connection
+DB = ActiveRecord::Base.connection
 
   if ENV["ACTIVE_RECORD_ENV"] == "test"
     ActiveRecord::Migration.verbose = false
